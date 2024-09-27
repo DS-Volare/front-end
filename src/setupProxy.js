8308129b48f9ae7ab81856de5baa42d9212bb6a4
unknown <lerle144@gmail.com>
2024-09-28 00:53:57 +0900

    [fix/design] 등장인물 형광펜 색 변경, 버그 수정 - 랜덤 색상이 끊임없이 렌더링되는 버그 수정

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -10,7 +10,7 @@ module.exports = function (app) {
   );
   app.use(
     createProxyMiddleware('/flask', {
-      target: 'http://75.63.212.242:44809',
+      target: 'http://116.102.163.165:31368',
       changeOrigin: true,
       pathRewrite: { '^/flask': '/' },
     })
